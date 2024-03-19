4925297ecbc5819b8d56e9b221ed428ced9ff828
Zijian Zhang <futrime@outlook.com>
2024-03-19 19:19:55 +0800

    refactor: optimize main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import datasets
 import pandas as pd
@@ -8,50 +9,56 @@ from dataset_metadata import DatasetMetadata
 from pattern import Pattern
 
 
-def load_dataset(metadata: DatasetMetadata) -> datasets.Dataset:
-    dataset = datasets.load_dataset(
-        path=metadata["path"],
-        name=metadata["name"],
-        split=metadata["split"],
-    )
+def load_datasets(metadata_list: List[DatasetMetadata]) -> pd.DataFrame:
+    dataset_list: List[datasets.Dataset] = []
 
+    for metadata in metadata_list:
+        dataset = datasets.load_dataset(
+            path=metadata["path"],
+            name=metadata["name"],
+            split=metadata["split"],
+        )
+
+        assert isinstance(dataset, datasets.Dataset)
+
+        dataset_list.append(
+            dataset.select_columns(
+                list(metadata["column_mapping"].keys()),
+            ).rename_columns(
+                metadata["column_mapping"],
+            )
+        )
+
+    dataset = datasets.concatenate_datasets(dataset_list)
     assert isinstance(dataset, datasets.Dataset)
 
-    return dataset.select_columns(
-        list(metadata["column_mapping"].keys()),
-    ).rename_columns(
-        metadata["column_mapping"],
-    )
+    dataframe = dataset.to_pandas()
+    assert isinstance(dataframe, pd.DataFrame)
+
+    return dataframe
 
 
-def match_pattern(dataframe: pd.DataFrame, pattern: Pattern) -> pd.DataFrame:
-    for index, row in dataframe.iterrows():
+def analyze_tags(dataframe: pd.DataFrame, pattern_list: List[Pattern]) -> pd.DataFrame:
+    dataframe["tags"] = [[] for _ in range(len(dataframe))]
+
+    for _, row in dataframe.iterrows():
         question = row["question"]
         answer = row["answer"]
         assert isinstance(question, str)
         assert isinstance(answer, str)
 
-        if pattern["matcher"](question, answer):
-            patterns = row["patterns"]
-            assert isinstance(patterns, list)
-            patterns.append(pattern["name"])
+        for pattern in pattern_list:
+            if pattern["matcher"](question, answer):
+                tags = row["tags"]
+                assert isinstance(tags, list)
+                tags.append(pattern["name"])
 
     return dataframe
 
 
 def main() -> None:
-    raw_dataset = datasets.concatenate_datasets(
-        [load_dataset(metadata) for metadata in DATASET_LIST],
-    )
-
-    dataset = raw_dataset.to_pandas()
-    assert isinstance(dataset, pd.DataFrame)
-
-    # Add patterns column
-    dataset["patterns"] = [[] for _ in range(len(dataset))]
-
-    for pattern in PATTERN_LIST:
-        dataset = match_pattern(dataset, pattern)
+    dataset = load_datasets(DATASET_LIST)
+    dataset = analyze_tags(dataset, PATTERN_LIST)
 
     os.makedirs("data/", exist_ok=True)
     dataset.to_csv("data/qa.csv")
