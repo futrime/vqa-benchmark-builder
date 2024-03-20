71cfc8cf1463989ba6d9eb2a34215d9ed3ce620c
Zijian Zhang <futrime@outlook.com>
2024-03-20 20:44:57 +0800

    feat: add more patterns

--- config.py
+++ config.py
@@ -3,149 +3,142 @@ from typing import List
 import datasets
 
 from dataset_metadata import DatasetMetadata
-from matchers import (
-    match_color,
-    match_counting,
-    match_object_factory,
-    match_shape,
-    match_spatial_relationship,
-    match_texture,
-)
+from matchers import *
 from pattern import Pattern
 
 DATASET_LIST: List[DatasetMetadata] = [
-    {
-        "path": "lmms-lab/ChartQA",
-        "name": None,
-        "split": "test",
-        "column_renaming": {
-            "question": "question",
-            "answer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/DocVQA",
-        "name": "DocVQA",
-        "split": "validation",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
-    {
-        "path": "lmms-lab/DocVQA",
-        "name": "InfographicVQA",
-        "split": "validation",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
-    {
-        "path": "lmms-lab/GQA",
-        "name": "train_all_instructions",
-        "split": "train",
-        "column_renaming": {
-            "question": "question",
-            "fullAnswer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/llava-bench-coco",
-        "name": None,
-        "split": "train",
-        "column_renaming": {
-            "question": "question",
-            "answer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/llava-bench-in-the-wild",
-        "name": None,
-        "split": "train",
-        "column_renaming": {
-            "question": "question",
-            "gpt_answer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/MP-DocVQA",
-        "name": None,
-        "split": "val",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
-    {
-        "path": "lmms-lab/OK-VQA",
-        "name": None,
-        "split": "val2014",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
-    {
-        "path": "flaviagiammarino/path-vqa",
-        "name": None,
-        "split": "train",
-        "column_renaming": {
-            "question": "question",
-            "answer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/textvqa",
-        "name": None,
-        "split": "validation",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
-    {
-        "path": "ruanchaves/visual7w-gpt",
-        "name": None,
-        "split": "train",
-        "column_renaming": {
-            "question": "question",
-            "answer": "answer",
-        },
-        "column_casting": {},
-    },
-    {
-        "path": "lmms-lab/VizWiz-VQA",
-        "name": None,
-        "split": "val",
-        "column_renaming": {
-            "question": "question",
-            "answers": "answer",
-        },
-        "column_casting": {
-            "answers": datasets.Value("string"),
-        },
-    },
+    # {
+    #     "path": "lmms-lab/ChartQA",
+    #     "name": None,
+    #     "split": "test",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/DocVQA",
+    #     "name": "DocVQA",
+    #     "split": "validation",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
+    # {
+    #     "path": "lmms-lab/DocVQA",
+    #     "name": "InfographicVQA",
+    #     "split": "validation",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
+    # {
+    #     "path": "lmms-lab/GQA",
+    #     "name": "train_all_instructions",
+    #     "split": "train",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "fullAnswer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/llava-bench-coco",
+    #     "name": None,
+    #     "split": "train",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/llava-bench-in-the-wild",
+    #     "name": None,
+    #     "split": "train",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "gpt_answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/MP-DocVQA",
+    #     "name": None,
+    #     "split": "val",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
+    # {
+    #     "path": "lmms-lab/OK-VQA",
+    #     "name": None,
+    #     "split": "val2014",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
+    # {
+    #     "path": "flaviagiammarino/path-vqa",
+    #     "name": None,
+    #     "split": "train",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/textvqa",
+    #     "name": None,
+    #     "split": "validation",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
+    # {
+    #     "path": "ruanchaves/visual7w-gpt",
+    #     "name": None,
+    #     "split": "train",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
+    # {
+    #     "path": "lmms-lab/VizWiz-VQA",
+    #     "name": None,
+    #     "split": "val",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "answers": "answer",
+    #     },
+    #     "column_casting": {
+    #         "answers": datasets.Value("string"),
+    #     },
+    # },
     {
         "path": "flaviagiammarino/vqa-rad",
         "name": None,
@@ -156,16 +149,16 @@ DATASET_LIST: List[DatasetMetadata] = [
         },
         "column_casting": {},
     },
-    {
-        "path": "lmms-lab/VQAv2",
-        "name": None,
-        "split": "validation",
-        "column_renaming": {
-            "question": "question",
-            "multiple_choice_answer": "answer",
-        },
-        "column_casting": {},
-    },
+    # {
+    #     "path": "lmms-lab/VQAv2",
+    #     "name": None,
+    #     "split": "validation",
+    #     "column_renaming": {
+    #         "question": "question",
+    #         "multiple_choice_answer": "answer",
+    #     },
+    #     "column_casting": {},
+    # },
 ]
 
 PATTERN_LIST: List[Pattern] = [
@@ -178,6 +171,10 @@ PATTERN_LIST: List[Pattern] = [
         "matcher": match_counting,
     },
     {
+        "name": "emotion",
+        "matcher": match_emotion,
+    },
+    {
         "name": "object",
         "matcher": match_object_factory(),
     },
@@ -186,11 +183,11 @@ PATTERN_LIST: List[Pattern] = [
         "matcher": match_shape,
     },
     {
-        "name": "texture",
-        "matcher": match_texture,
-    },
-    {
         "name": "spatial relationship",
         "matcher": match_spatial_relationship,
+    },
+    {
+        "name": "texture",
+        "matcher": match_texture,
     },
 ]
